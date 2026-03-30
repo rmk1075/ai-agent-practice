24d82acf27c660b85dcd0a15aace4c5f5c349589
jaeminjeon <rmk1075@gmail.com>
2026-03-30 19:20:44 +0900

    feat: remove unused chat app

--- app/backend/agent/agent/settings.py
+++ app/backend/agent/agent/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'chat',
     'conversation',
 ]
 
--- app/backend/agent/chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from chat.views import chat_stream
-
-urlpatterns = [
-    path("stream/", chat_stream)
-]
