54244bae2a839f2b2584775ab969261e2e492227
jaeminjeon <rmk1075@gmail.com>
2026-03-30 13:47:52 +0900

    feat: define BaseModel

---
+++ app/backend/agent/agent/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+        return 1, {self._meta.label: 1}
