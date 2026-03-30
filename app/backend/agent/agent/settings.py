5ead23033076bbc28718431d548fae6cd811b883
jaeminjeon <rmk1075@gmail.com>
2026-03-30 13:47:40 +0900

    feat: add rest_framework, conversation to INSTALLED_APPS

--- app/backend/agent/agent/settings.py
+++ app/backend/agent/agent/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'chat'
+    'rest_framework',
+    'chat',
+    'conversation',
 ]
 
 MIDDLEWARE = [
