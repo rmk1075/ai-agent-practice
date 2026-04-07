246ad1c5badfac2a44bbd56ef627f72149abb883
jaeminjeon <rmk1075@gmail.com>
2026-04-07 10:27:32 +0900

    refactor: remove duplicate env loading - manage.py - settings.py

--- app/backend/agent/manage.py
+++ app/backend/agent/manage.py
@@ -3,13 +3,9 @@
 import os
 import sys
 
-from dotenv import load_dotenv
-
 
 def main():
     """Run administrative tasks."""
-
-    load_dotenv()
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agent.settings')
     try:
